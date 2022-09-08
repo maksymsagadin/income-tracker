da4a632ac1a4c33e8b224c7c02829e81cb95e203
Maksym Sagadin <maxsagadin@gmail.com>
2022-09-07 17:19:35 -0700

    added useContext and useReducer to manage data

---
+++ src/context/context.js
@@ -0,0 +1,23 @@
+import React, { useReducer, createContext } from 'react'
+import contextReducer from './contextReducer'
+
+const initialState = JSON.parse(localStorage.getItem('transactions')) || [{ amount: 3000, category: 'Salary', type: 'Income', date: '2020-02-20', id: '44c68123-5b86-4cc8-b915-bb9e16cebe6a' }, { amount: 3000, category: 'Rent', type: 'Expense', date: '2020-11-16', id: '33b295b8-a8cb-49f0-8f0d-bb268686de1a' }]
+
+export const ExpenseTrackerContext = createContext(initialState)
+
+export const Provider = ({ children }) => {
+    const [transactions, dispatch] = useReducer(contextReducer, initialState)
+
+    // Actions
+    const addTransaction = (transaction) => {
+        dispatch({ type: 'ADD_TRANSACTION', payload: transaction })
+    }
+    const deleteTransaction = (id) => {
+        dispatch({ type: 'DELETE_TRANSACTION', payload: id })
+    }
+    return (
+        <ExpenseTrackerContext.Provider value={{ transactions, addTransaction, deleteTransaction }}>
+            {children}
+        </ExpenseTrackerContext.Provider>
+    )
+}
