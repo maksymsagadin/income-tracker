de380c5539cf1f36dc85ec8163322fc5c9921055
Maksym Sagadin <maxsagadin@gmail.com>
2022-09-07 18:06:20 -0700

    included deleteTransaction feature on delete icon

--- src/components/Main/List/List.jsx
+++ src/components/Main/List/List.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { List as MUIList, ListItem, ListItemAvatar, Avatar, ListItemText, ListItemSecondaryAction, IconButton, Slide } from '@material-ui/core'
 import { Delete, MoneyOff } from '@material-ui/icons'
 
+import { ExpenseTrackerContext } from '../../../context/context'
 import useStyles from './styles'
 const List = () => {
     const classes = useStyles()
-    const transactions = [
-        { id: 1, type: 'Income', category: 'Salary', amount: 500, date: new Date(), }
-    ]
+    const { transactions, deleteTransactions } = useContext(ExpenseTrackerContext)
+    
     return (
         <MUIList dense={false} className={classes.list}>
             {transactions.map((transaction) => (
@@ -20,7 +20,7 @@ const List = () => {
                         </ListItemAvatar>
                         <ListItemText primary={transaction.category} secondary={`$${transaction.amount} - ${transaction.date}`} />
                         <ListItemSecondaryAction>
-                            <IconButton edge="end" aria-label="delete" onClick=''>
+                            <IconButton edge="end" aria-label="delete" onClick={() => deleteTransactions(transaction.id)}>
                                 <Delete />
                             </IconButton>
                         </ListItemSecondaryAction>
