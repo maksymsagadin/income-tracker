2f630dd963697ede6203b627e2b48fd54e1eb3f0
Maksym Sagadin <maxsagadin@gmail.com>
2022-09-08 17:31:06 -0700

    updated deprecated prop variant

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import useStyles from './styles'
 const App = () => {
     const classes = useStyles()
     return (
-        <Grid className={classes.grid} container spacing={0} alignItems='center' justify='center' style={{ height: '100vh' }}>
+        <Grid className={classes.grid} container spacing={0} alignItems='center' justifyContent='center' style={{ height: '100vh' }}>
             <Grid item xs={12} sm={3} className={classes.desktop}>
                 <Details title='Income' />
             </Grid>
