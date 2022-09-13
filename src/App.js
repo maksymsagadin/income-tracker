5d617e167653a5a54cfe6a074ef5027e347f8f66
Maksym Sagadin <maxsagadin@gmail.com>
2022-09-12 17:46:16 -0700

    added push to talk button at bottom of page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Grid } from '@material-ui/core'
-
+import { PushToTalkButton, PushToTalkButtonContainer } from '@speechly/react-ui'
 import Details from './components/Details/Details'
 import Main from './components/Main/Main'
 
@@ -9,21 +9,26 @@ import useStyles from './styles'
 const App = () => {
     const classes = useStyles()
     return (
-        <Grid className={classes.grid} container spacing={0} alignItems='center' justifyContent='center' style={{ height: '100vh' }}>
-            <Grid item xs={12} sm={3} className={classes.desktop}>
-                <Details title='Income' />
+        <>
+            <Grid className={classes.grid} container spacing={0} alignItems='center' justifyContent='center' style={{ height: '100vh' }}>
+                <Grid item xs={12} sm={3} className={classes.desktop}>
+                    <Details title='Income' />
+                </Grid>
+                <Grid item xs={12} sm={4} className={classes.main}>
+                    <Main />
+                </Grid>
+                <Grid item xs={12} sm={3} className={classes.mobile}>
+                    <Details title='Income' />
+                </Grid>
+                <Grid item xs={12} sm={3} className={classes.last}>
+                    <Details title='Expense' />
+                </Grid>
             </Grid>
-            <Grid item xs={12} sm={4} className={classes.main}>
-                <Main />
-            </Grid>
-            <Grid item xs={12} sm={3} className={classes.mobile}>
-                <Details title='Income' />
-            </Grid>
-            <Grid item xs={12} sm={3} className={classes.last}>
-                <Details title='Expense' />
-            </Grid>
-        </Grid>
-  )
+            <PushToTalkButtonContainer>
+                <PushToTalkButton />
+            </PushToTalkButtonContainer>
+        </>
+    )
 }
 
 export default App
