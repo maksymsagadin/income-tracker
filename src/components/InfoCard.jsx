ab705b3bbb8fd80fb110bbc18617934a73863904
Maksym Sagadin <maxsagadin@gmail.com>
2022-09-16 11:58:47 -0700

    updated info card suggestion text

--- src/components/InfoCard.jsx
+++ src/components/InfoCard.jsx
@@ -5,7 +5,7 @@ const isIncome = Math.round(Math.random());
 const InfoCard = () => {
   return (
     <div elevation={3} style={{ textAlign: 'center' }}>
-      Try saying: <br /> 
+      Try Saying: <br /> 
       Add {isIncome ? 'Income ' : 'Expense '} 
       for {isIncome ? '$1000 ' : '$500 '}  
       in Category {isIncome ? 'Salary ' : 'Food '}
