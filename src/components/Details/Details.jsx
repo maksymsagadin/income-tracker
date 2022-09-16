a41a5d0cb301d945dd88c66021c3302f659e8210
Maksym Sagadin <maxsagadin@gmail.com>
2022-09-16 11:54:07 -0700

    added styling divider component

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Card, CardHeader, CardContent, Typography } from '@material-ui/core'
+import { Card, CardHeader, CardContent, Divider, Typography } from '@material-ui/core'
 import { Doughnut } from 'react-chartjs-2'
 import useStyles from './styles.js'
 import useTransactions from '../../useTransactions.js'
@@ -10,6 +10,7 @@ const Details = ({ title }) => {
     return (
         <Card className={title === 'Income' ? classes.income : classes.expense}>
             <CardHeader title={title}/>
+            <Divider variant="middle"/>
             <CardContent>
                 <Typography variant='h5'>${total}</Typography>
                 <Doughnut data={chartData} />
